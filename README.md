a867887715dc350b26007547338cea160d56fc68
MagretBro <magretbro@gmail.com>
2025-02-06 08:06:41 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # LearnRxjs
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.6.
+and based on the awesome youtube lesson: https://www.youtube.com/watch?v=PXoWHqKtHGA
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,61 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learn-rxjs';
+
+  
+  ngOnInit(): void {
+    this.promiseTest();
+  }
+
+
+
+  // в этом примере из-за промиса нет вывода с задержкой, т к промис
+  getPromiseAsync(): Promise<number>
+  {
+    return new Promise<number>(resolve =>
+      {
+        console.log('Promise work');
+        setTimeout(() => resolve(1), 2000);
+
+       resolve(3);
+      }
+    );
+  }
+
+promiseTest(): void{
+  const p = this.getPromiseAsync();
+
+  p.then(_ =>
+  { console.log(`Promise then ${_}`);
+  });
+  p.catch(_ =>
+  {
+    console.log('Promise catch');
+  });
+  console.log('Promise execute func end');
 }
+}
+
+
+
+// simple exp Promise
+
+  // someObservable = new Observable((sub) => {
+  //   console.log(':)');
+  //   sub.next('Smth datas');
+  // });
+
+  // ngOnInit() {
+  //   this.someObservable.subscribe(console.log);
+  //   this.someObservable.subscribe((value) => console.log('2 next', value));
+  //   this.someObservable.subscribe(
+  //     {next: (value) => console.log('3 next', value),
+  //     });
+  //   this.someObservable.subscribe({
+  //     next(value) {
+  //       console.log('4 next:', value);
+  //     },
+  //   });
+  // }
+
+  //}
